e703ab885db78b9d8ce1828883a4b10237dc5a7c
Dan Koskei <qimtaid@gmail.com>
2024-03-31 15:37:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+const superbowlWin = (record) => {
+    // Use the find method
+    const winRecord = record.find((game) => game.result === "W");
+  
+    // If a win record is found, return the year. Otherwise, return undefined.
+    return winRecord ? winRecord.year : undefined;
+  };
